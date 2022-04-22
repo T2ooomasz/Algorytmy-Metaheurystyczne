7972b51421f35b5127ab754b47bcfbd8720c4025
T2ooomasz <tomkazulak@gmail.com>
2022-04-22 18:31:33 +0200

    Lab 3 update

--- TabuSearch/TSP_functions.py
+++ TabuSearch/TSP_functions.py
@@ -1,2 +1,57 @@
-def calc(x, y):
-    return x + y
+import math
+import random
+import tsplib95
+from tsplib95 import distances
+import numpy as np
+import matplotlib.pyplot as plt
+
+def initialize_problem():
+    number_of_cities = 10
+    min_distance = 10
+    max_distance = 100
+    seed = 0
+    np.random.seed(seed)
+    rand_matrix = np.random.randint(min_distance, max_distance + 1, size=(number_of_cities, number_of_cities))
+    for i in range(number_of_cities):
+        rand_matrix[i][i] = 0
+        for j in range(number_of_cities):
+            rand_matrix[j][i] = rand_matrix[i][j]
+    return number_of_cities, rand_matrix
+
+def random_solution(n):
+    tour = list(range(n))
+    random.shuffle(tour)
+    return tour
+
+
+def get_weight(tour, distance_matrix):
+    weight = distance_matrix[tour[0]][tour[-1]]
+    for i in range(1, len(tour)):
+        weight += distance_matrix[tour[i]][tour[i - 1]]
+    return weight
+
+def swap_positions(tour, i, j):
+    tour[i], tour[j] = tour[j], tour[i]
+    return tour
+
+def invert(tour, i, j):
+    length_sub_list = j - i
+    current_tour = tour.copy()
+    for k in range(int(length_sub_list / 2 + 1)):
+        current_tour = swap_positions(current_tour, i, j - k)
+        i += 1
+    return current_tour
+
+# something loke this but better!
+def get_neighbors(best_candidate, number_of_cities):
+    Neighborhood = np.array([0,0,0,0,0,0,0,0,0,0])
+    i = 0
+    while i < number_of_cities:
+        j = i+1
+        while j < number_of_cities:
+            best_candidate_copy = best_candidate.copy()
+            best_candidate_copy = invert(best_candidate_copy, i, j)
+            Neighborhood = np.vstack([Neighborhood, best_candidate_copy])
+            j += 1
+        i += 1
+    return Neighborhood
--- TabuSearch/Tabu_Search.py
+++ TabuSearch/Tabu_Search.py
@@ -1,14 +1,29 @@
+#import numpy as np
+
 import TSP_functions as tsp
 
-def Step1():
-    best_solution = random
-    best_candidate = best_solution
-    tabuList = []
-    tabuList.append = best_solution
-
 def main():
-    x = tsp.calc(4,9)
-    print(x)
+    # Step 1:
+    size_of_problem, matrix_tsp = tsp.initialize_problem()
+    best_solution = tsp.random_solution(size_of_problem)
+    best_candidate = best_solution
+    #TabuList = np.array([])
+    
+    # Step 2:
+    stop_condition = False
+    i = 0
+    while(not stop_condition):
+        Neighborhood = tsp.get_neighbors(best_candidate, size_of_problem)
+        print(Neighborhood)
+        best_candidate = Neighborhood[0]
+        print("=================================================")
+        print(best_candidate)
+        #for candidate in Neighborhood:
+            #if ( (not tabuList.contains(sCandidate)) and (fitness(sCandidate) > fitness(bestCandidate)) )
+            #bestCandidate ← sCandidate
+        if i > 2:
+            stop_condition = True
+        i += 1
 
 if __name__ == '__main__':
     main()
