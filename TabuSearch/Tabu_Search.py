2579b4f7b19ef14c9796f2a30ff799375637ce4d
T2ooomasz <tomkazulak@gmail.com>
2022-04-22 17:20:30 +0200

    Lab 3

---
+++ TabuSearch/Tabu_Search.py
@@ -0,0 +1,14 @@
+import TSP_functions as tsp
+
+def Step1():
+    best_solution = random
+    best_candidate = best_solution
+    tabuList = []
+    tabuList.append = best_solution
+
+def main():
+    x = tsp.calc(4,9)
+    print(x)
+
+if __name__ == '__main__':
+    main()
