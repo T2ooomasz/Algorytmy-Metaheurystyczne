4419bd86e775354a3db3a4228c7d4b3961452d6e
T2ooomasz <tomkazulak@gmail.com>
2022-05-30 22:34:17 +0200

    nothing new

--- Population/alg/crossing.py
+++ Population/alg/crossing.py
@@ -22,7 +22,7 @@ class crossing():
         elif(self.type_of_crossing == 'PMX'):
             self.crossing_PMX()
         else:
-            print("Working, but there is no \"", self.type_of_crossing, "\" type of crossing" )
+            print("There is no \"", self.type_of_crossing, "\" type of crossing" )
 
     def crossing_HX(self):
         pass
@@ -31,31 +31,39 @@ class crossing():
         pass
 
     def crossing_PMX(self):
+        '''
+        for [parent1, parent2] in Parents:
+            Child1, Child2 = PMX(Parent1, Parent2, cut_points)
+            childrens.append(Child1, Child2)
+
+        return childrens
+        '''
         pass
 
-    def PMX(Parent1, Parent2, cut_points):  # para rodziców - może być inaczej przekazana
+    def PMX(self, Parent1, Parent2, cut_points):  # para rodziców - może być inaczej przekazana
         Child1 = np.zeros(8, dtype=int)
         Child2 = np.zeros(8, dtype=int)
         Child1[cut_points[0]:cut_points[1]] = Parent2[cut_points[0]:cut_points[1]]
         Child2[cut_points[0]:cut_points[1]] = Parent1[cut_points[0]:cut_points[1]]
-        Child1 = PMX_cross(Parent1, Child1, cut_points)
-        CHild2 = PMX_cross(Parent2, Child2, cut_points)
+        Child1 = self.PMX_cross(Parent1, Child1, cut_points)
+        CHild2 = self.PMX_cross(Parent2, Child2, cut_points)
+        return Child1, Child2
 
 
-    def PMX_cross(Parent, Child, cut_points):
-        Child = filling(Parent, Child, cut_points)
-        Child = filling_maping(Parent, Child, cut_points)
+    def PMX_cross(self, Parent, Child, cut_points):
+        Child = self.filling(Parent, Child, cut_points)
+        Child = self.filling_mapping(Parent, Child, cut_points)
         return Child
 
-    def mapping_(Parent, Child, numb):
+    def mapping_(self, Parent, Child, numb):
         if numb in Child:
             j = 0
             while Child[j] != numb:
                 j += 1
-            numb = mapping_(Parent, Child, Parent[j])
+            numb = self.mapping_(Parent, Child, Parent[j])
         return numb
 
-    def filling(Parent, Child, cut_points):
+    def filling(self, Parent, Child, cut_points):
         for i in range(0, cut_points[0]):
             if Parent[i] not in Child:
                 Child[i] = Parent[i]
@@ -64,17 +72,17 @@ class crossing():
                 Child[i] = Parent[i]
         return Child
 
-    def filling_mapping(Parent, Child, cut_points):
+    def filling_mapping(self, Parent, Child, cut_points):
         for i in range(cut_points[0]):
             if Parent[i] in Child and Parent[i] != Child[i]:
                 j = 0
                 while Child[j] != Parent[i]:
                     j += 1
-                Child[i] = mapping_(Parent, Child, Parent[i])
+                Child[i] = self.mapping_(Parent, Child, Parent[i])
         for i in range(cut_points[1], len(Parent)):
             if Parent[i] in Child and Parent[i] != Child[i]:
                 j = 0
                 while Child[j] != Parent[i]:
                     j += 1
-                Child[i] = mapping_(Parent, Child, Parent[i])
+                Child[i] = self.mapping_(Parent, Child, Parent[i])
         return Child
