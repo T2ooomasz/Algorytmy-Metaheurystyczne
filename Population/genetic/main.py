b5e6daeeeb968f6b5c352394e538e3b088870de0
Zapalka <93160769+PiotrZapala@users.noreply.github.com>
2022-06-07 18:15:18 +0200

    Update main.py

--- Population/genetic/main.py
+++ Population/genetic/main.py
@@ -1,3 +1,11 @@
+from random import choice, randint, uniform
+import time
+from pandas import array
+import tsp_initialize as init
+import tsp_functions as func
+import population
+import genetic_algorithm
+
 if __name__ == '__main__':
     pop = population(200)
     parents = pop.initialize_population()
@@ -11,3 +19,12 @@ if __name__ == '__main__':
     for i in range(len(new_population)):
         phenotypes1.append(new_population[i].phenotype)
     print(min(phenotypes1))
+    #pop = population(200) 
+    #set_of_individuals = pop.initialize_population()
+    #sel = selection('random', set_of_individuals)
+    #fathers = sel.selected_fathers
+    #mothers = sel.selected_mothers
+    #cro = crossing('PMX', fathers, mothers)
+    #children = cro.children
+    #sel_pop = selection_population('roulette', set_of_individuals, children)
+    #new_population = sel_pop.new_population
