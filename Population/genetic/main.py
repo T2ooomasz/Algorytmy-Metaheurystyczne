6004170c85795cfa8630bbf0b238cd26a9daa6c2
Zapalka <93160769+PiotrZapala@users.noreply.github.com>
2022-06-07 17:41:47 +0200

    Update main.py

--- Population/genetic/main.py
+++ Population/genetic/main.py
@@ -1,1 +1,13 @@
-
+if __name__ == '__main__':
+    pop = population(200)
+    parents = pop.initialize_population()
+    phenotypes = []
+    for i in range(200):
+        phenotypes.append(parents[i].phenotype)
+    print(min(phenotypes))   
+    gen = genetic_algorithm('roulette', 'roulette', 'PMX', parents)
+    new_population = gen.parents
+    phenotypes1 = []
+    for i in range(len(new_population)):
+        phenotypes1.append(new_population[i].phenotype)
+    print(min(phenotypes1))
