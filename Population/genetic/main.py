cb27536e3d901e900cf1024396879a8b94389b93
Zapalka <93160769+PiotrZapala@users.noreply.github.com>
2022-06-08 20:59:20 +0200

    Update main.py

--- Population/genetic/main.py
+++ Population/genetic/main.py
@@ -6,25 +6,48 @@ import tsp_functions as func
 import population
 import genetic_algorithm
 
+def main():
+    condition = True
+    print()
+    print("Genetic Algorithm for 'Travelling salesman problem'\n")
+    print("+++++++++++++++++++++++++++++++++++++++++++++++++++")
+    while condition:
+        print()
+        print("To continue press '1'\nTo exit press '0")
+        option = int(input().strip())
+        if option == 1:
+            print("Enter size of population:")
+            size_of_population = int(input().strip())
+            print("Enter the type of selection individuals for the crossing:")
+            print("1.random\n2.roulette\n3.tournament")
+            type_of_selection = str(input().strip())
+            print("Enter the type of selection the new generation:")
+            print("1.random\n2.roulette\n3.tournament")
+            type_of_selection_population = str(input().strip())
+            print("Enter the type of the crossing:")
+            print("1.HX\n2.OX\n3.PMX")
+            type_of_crossing = str(input().strip())
+            print("Enter the type of the mutation:")
+            print("1.swap\n2.invert")
+            type_of_mutation = str(input().strip())
+            print("Enter the probability of the mutation:")
+            probability_of_mutation = float(input().strip())
+            print("+++++++++++++++++++++++++++++++++++++++++++++++++++")
+            best = []
+            pop = population(size_of_population)
+            parents = pop.set_of_individuals
+            best_solutions = pop.best_solution
+            size_of_population = pop.size_of_population
+            distance_matrix = pop.distance_matrix
+            gen = genetic_algorithm(str(type_of_selection), str(type_of_selection_population), str(type_of_crossing), str(type_of_mutation), probability_of_mutation, size_of_population, parents, best_solutions, distance_matrix)
+            new_population = gen.parents
+            best_individual = min(gen.best_solutions)
+            print(best_individual)
+            for i in range(len(parents)):
+                best.append(new_population[i].phenotype)
+            print(min(best))
+        else:
+            condition = False
+
 if __name__ == '__main__':
-    pop = population(200)
-    parents = pop.initialize_population()
-    phenotypes = []
-    for i in range(200):
-        phenotypes.append(parents[i].phenotype)
-    print(min(phenotypes))   
-    gen = genetic_algorithm('roulette', 'roulette', 'PMX', parents)
-    new_population = gen.parents
-    phenotypes1 = []
-    for i in range(len(new_population)):
-        phenotypes1.append(new_population[i].phenotype)
-    print(min(phenotypes1))
-    #pop = population(200) 
-    #set_of_individuals = pop.initialize_population()
-    #sel = selection('random', set_of_individuals)
-    #fathers = sel.selected_fathers
-    #mothers = sel.selected_mothers
-    #cro = crossing('PMX', fathers, mothers)
-    #children = cro.children
-    #sel_pop = selection_population('roulette', set_of_individuals, children)
-    #new_population = sel_pop.new_population
+    main()
