6582b33880f3ee2184ff7a4fef481d6eb81154fb
Zapalka <93160769+PiotrZapala@users.noreply.github.com>
2022-06-08 21:14:08 +0200

    Update mutation.py

--- Population/genetic/mutation.py
+++ Population/genetic/mutation.py
@@ -15,23 +15,23 @@ class mutation:
 
     def mutation_algorithm(self):
         if self.type_of_mutation == 'swap':
-            self.mutation_0()
+            self.swap_mutation()
         elif self.type_of_mutation == 'invert':
-            self.mutation_1()
+            self.invert_mutation()
         else:
             print("There is no \"", self.type_of_mutation, "\" type of mutation")
 
-    def mutation_0(self):
+    def swap_mutation(self):
         r = uniform(0, 1)
         if r <= self.probability_of_mutation:
-            for _ in range(20):
+            for i in range(20):
                 rand = randint(0, self.size_of_population-1)
                 rand1 = randint(0, len(self.set_of_individuals[rand].genotype)-1)
                 rand2 = randint(0, len(self.set_of_individuals[rand].genotype)-1)
                 func.swap_positions(self.set_of_individuals[rand].genotype, rand1, rand2)
         return self.set_of_individuals
 
-    def mutation_1(self):
+    def invert_mutation(self):
         r = uniform(0, 1)
         if r <= self.probability_of_mutation:
             for _ in range(5):
