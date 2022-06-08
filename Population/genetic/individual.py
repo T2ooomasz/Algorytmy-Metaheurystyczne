0f14f155d02ffe006d0dc601dd0fbdddc895db10
Zapalka <93160769+PiotrZapala@users.noreply.github.com>
2022-06-08 20:58:33 +0200

    Update individual.py

--- Population/genetic/individual.py
+++ Population/genetic/individual.py
@@ -1,6 +1,6 @@
 class individual:
 
-    def __init__(self, genotype, phenotype):
+    def __init__(self, genotype, phenotype, fitness):
         self.genotype = genotype
         self.phenotype = phenotype
-        
+        self.fitness = fitness
