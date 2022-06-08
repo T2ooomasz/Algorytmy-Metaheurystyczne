55553ef26d3cf4a3f497eb6508d68b8dc9e57ab7
Zapalka <93160769+PiotrZapala@users.noreply.github.com>
2022-06-08 20:59:51 +0200

    Update mutation.py

--- Population/genetic/mutation.py
+++ Population/genetic/mutation.py
@@ -6,22 +6,30 @@ import tsp_functions as func
 
 class mutation:
 
-    def __init__(self, type_of_mutation = '', set_of_individuals = []):
+    def __init__(self, size_of_population, probability_of_mutation, type_of_mutation = '', set_of_individuals = []):
+        self.size_of_population = size_of_population
+        self.probability_of_mutation = probability_of_mutation
         self.type_of_mutation = type_of_mutation
         self.set_of_individuals = set_of_individuals 
         self.mutation_algorithm()
 
     def mutation_algorithm(self):
-        if self.type_of_mutation == '0':
+        if self.type_of_mutation == 'swap':
             self.mutation_0()
-        elif self.type_of_mutation == '1':
+        elif self.type_of_mutation == 'invert':
             self.mutation_1()
         else:
             print("There is no \"", self.type_of_mutation, "\" type of mutation")
 
     def mutation_0(self):
-        pass
-        #losowy swap       
+        r = uniform(0, 1)
+        if r <= self.probability_of_mutation:
+            for i in range(20):
+                rand = randint(0, self.size_of_population-1)
+                rand1 = randint(0, len(self.set_of_individuals[rand].genotype)-1)
+                rand2 = randint(0, len(self.set_of_individuals[rand].genotype)-1)
+                func.swap_positions(self.set_of_individuals[rand].genotype, rand1, rand2)
+        return self.set_of_individuals
 
     def mutation_1(self):
         pass
